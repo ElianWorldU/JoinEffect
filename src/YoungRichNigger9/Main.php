5405784fd9fab85ca3233bd67007f1c84a328432
YoungRichNigger9 <francoistranchot@gmail.com>
2016-08-21 02:49:36 +0200

    Update Main.php

--- src/YoungRichNigger9/Main.php
+++ src/YoungRichNigger9/Main.php
@@ -1,1 +1,27 @@
 
+namespace YoungRichNigger9;
+
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+use pocketmine\Command\Command;
+use pocketmine\Command\CommandSender;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\entity\Effect;
+use pocketmine\utils\TextFormat;
+
+class Main extends PluginBase implements Listener {
+ public function onEnable(){
+	$this->saveDefaultConfig();
+	$this->reloadConfig();
+	$this->getServer()->getPluginManager()->registerEvents($this,$this);
+	$this->getLogger()->info("§eJoinEffect §aEnabled!");
+    }
+  public function onLoad(){
+  $this->getLogger()->info("§eJoinEffect §bLoading");
+  }
+  public function onDisable(){
+  $this->getLogger()->info("§eJoinEffect §cDisable");
+ }
+ 

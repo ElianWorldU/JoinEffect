ddeff2c51d541f2522a52c7e9d1ed0624704245f
YoungRichNigger9 <francoistranchot@gmail.com>
2016-08-22 11:46:07 +0200

    Update Main.php

--- src/YoungRichNigger9/Main.php
+++ src/YoungRichNigger9/Main.php
@@ -11,39 +11,57 @@ use pocketmine\Command\CommandSender;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\entity\Effect;
 use pocketmine\event\Listener;
+use pocketmine\Server;
 
 class Main extends PluginBase implements Listener {
- public function onEnable(){
-	$this->saveDefaultConfig();
-	$this->reloadConfig();
-	$this->getServer()->getPluginManager()->registerEvents($this,$this);
-	$this->getLogger()->info("§eJoinEffect §aEnabled!");
+
+    public function onEnable() {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getLogger()->info("§eJoinEffect §aEnabled!");
+        $cfg = new Config($this->getConfig() . "config.yml", Config::YAML, array(
+            "Effect-id" => 1,
+            "Effect-Duration" => 2400,
+            "Amplifier" => 0,
+            "Particles" => true
+        ));
     }
-  public function onLoad(){
-  $this->getLogger()->info("§eJoinEffect §bLoading");
-  }
-  public function onDisable(){
-  $this->getLogger()->info("§eJoinEffect §cDisable");
- }
+
+    public function onLoad() {
+        $this->getLogger()->info("§eJoinEffect §bLoading");
+    }
+
+    public function onDisable() {
+        $this->getLogger()->info("§eJoinEffect §cDisable");
+    }
+
     public function receiveEffect() {
         $cfg = $this->getConfig();
-        if (!$sender instanceof Player) {
-            $sender->sendMessage("§4You §lSERIOUSLY§r§4 think that you can add the Console effects?");
-        } else {
-            $p = $this->getServer()->getPlayer();
-            if ($p->hasPermission("joineffect.cmd")) {
-                $effectid = $cfg->get("Effect-ID");
-                $duration = $cfg->get("Duration");
-                $particles = $cfg->get("Particles");
-                $amplifier = $cfg->get("Amplifier");
-                $health = $cfg->get("Fill-Player-Health");
-                $effect = Effect::getEffect($effectid);
-                $effect->setVisible($particles);
-                $effect->setAmplifier($amplifier);
-                $effect->setDuration($duration);
-                $p->addEffect($effect);
-            }
+        $player = $this->getServer()->getPlayer();
+        if ($player->hasPermission("joineffect.cmd")) {
+            $id = $cfg->get("Effect-ID");
+            $duration = $cfg->get("Duration");
+            $particles = $cfg->get("Particles");
+            $amplifier = $cfg->get("Amplifier");
+            $player = Effect::getEffect($id);
+            $player->setVisible($particles);
+            $player->setAmplifier($amplifier);
+            $player->setDuration($duration);
+            $player->addEffect($id);
         }
     }
+
+    public function onJoin(PlayerJoinEvent $event) {
+        $cfg = $this->getConfig();
+        $id = $cfg->get("Effect-id");
+        $ticks = $cfg->get("Effect-Duration");
+        $amplifier = $cfg->get("Amplifier");
+        $particle = $cfg->get("Particles");
+        $player = $event->getPlayer();
+        $player->getEffect($id);
+        $player->setDuration($ticks);
+        $player->setAmplifier($amplifier);
+        $player->setVisible($particle);
+        $player->addEffect($id);
+    }
+
 }
- 

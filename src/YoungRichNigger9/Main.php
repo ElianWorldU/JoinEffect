f799ef89d265f3c2d30ba37d5940cdeb7f68fe36
Hunyahemee <kiethung123@gmail.com>
2016-08-22 17:15:52 +0700

    Changed to maybe working Main.php
    Try now

--- src/YoungRichNigger9/Main.php
+++ src/YoungRichNigger9/Main.php
@@ -11,14 +11,13 @@ use pocketmine\Command\CommandSender;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\entity\Effect;
 use pocketmine\event\Listener;
-use pocketmine\Server;
 
 class Main extends PluginBase implements Listener {
 
     public function onEnable() {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->getLogger()->info("§eJoinEffect §aEnabled!");
-        $cfg = new Config($this->getConfig() . "config.yml", Config::YAML, array(
+        $cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML, array(
             "Effect-id" => 1,
             "Effect-Duration" => 2400,
             "Amplifier" => 0,
@@ -27,41 +26,31 @@ class Main extends PluginBase implements Listener {
     }
 
     public function onLoad() {
-        $this->getLogger()->info("§eJoinEffect §bLoading");
+        $this->getLogger()->info(TextFormat::YELLOW ."JoinEffect ". TextFormat::BLUE ."Loading");
     }
 
     public function onDisable() {
-        $this->getLogger()->info("§eJoinEffect §cDisable");
-    }
-
-    public function receiveEffect() {
-        $cfg = $this->getConfig();
-        $player = $this->getServer()->getPlayer();
-        if ($player->hasPermission("joineffect.cmd")) {
-            $id = $cfg->get("Effect-ID");
-            $duration = $cfg->get("Duration");
-            $particles = $cfg->get("Particles");
-            $amplifier = $cfg->get("Amplifier");
-            $player = Effect::getEffect($id);
-            $player->setVisible($particles);
-            $player->setAmplifier($amplifier);
-            $player->setDuration($duration);
-            $player->addEffect($id);
-        }
+        $this->getLogger()->info(TextFormat::YELLOW ."JoinEffect ". TextFormat::RED ."Disabled");
+        $cfg->save();
     }
 
     public function onJoin(PlayerJoinEvent $event) {
-        $cfg = $this->getConfig();
         $id = $cfg->get("Effect-id");
         $ticks = $cfg->get("Effect-Duration");
         $amplifier = $cfg->get("Amplifier");
         $particle = $cfg->get("Particles");
         $player = $event->getPlayer();
-        $player->getEffect($id);
-        $player->setDuration($ticks);
-        $player->setAmplifier($amplifier);
-        $player->setVisible($particle);
-        $player->addEffect($id);
+        
+        $effect = Effect::getEffect($id);
+          
+        $effect->setDuration($ticks);
+        $effect->setAmplifier($amplifier);
+        $effect->setVisible($particle);
+        
+        if($player->hasPermission("joineffect.permission")) {
+             $player->addEffect($effect);
+            $player->sendMessage(TextFormat::YELLOW ."[JoinEffect] ". TextFormat::GREEN ."You recieved Effect!");
+         }
     }
 
 }
